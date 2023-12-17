2b4c311d871f35a0a66c0e341ca325e6350ef820
moiz0304 <101109026+moiz0304@users.noreply.github.com>
2023-12-17 20:10:41 +0500

    Update keys.py

--- DSA Project/keys.py
+++ DSA Project/keys.py
@@ -1,5 +1,5 @@
 cookie_dict = {
-    "__Secure-1PSID": "cwjt7xHFB90WI1UL1bT3ErAkdFjquOmzgqSgYspQgAzSVGHxaiYBn6CQiYkOSCkH3aWRzw.",
-    "__Secure-1PSIDTS": "sidts-CjIBNiGH7jD76lTERue1zGM91JmEKgc5uduKmZOV-zYI7BDK0C6R6O_Ns4IZH6q6spZVzRAA",
+    "__Secure-1PSID": "your-key-here",
+    "__Secure-1PSIDTS": "your-key-here",
     # Any cookie values you want to pass session object.
 }

720db8a9149467db0f160e9c568ba95edf198dab
moiz0304 <101109026+moiz0304@users.noreply.github.com>
2023-12-17 20:11:44 +0500

    Delete DSA Project/main v0.py

--- DSA Project/main v0.py
+++
@@ -1,8 +0,0 @@
-from bardapi import Bard
-from bardapi import BardCookies
-from keys import cookie_dict
-bard = BardCookies(cookie_dict=cookie_dict)
-
-image = open('pic.jpg', 'rb').read() # (jpeg, png, webp) are supported.
-bard_answer = bard.ask_about_image('Give a one line Image Description in English', image)
-print(bard_answer['content'])
